38653f18e2bccb715a87fc28bcd324cc80ce3470
Hannah King <hmking.nc@gmail.com>
2016-12-20 22:29:09 -0500

    styling updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
 			emerald = randomizer(1, 12);
 			wins = 0;
 			losses = 0;
+			total = 0;
 			
 		console.log(numberRand, diamond, ruby, cobalt, emerald);
 		
@@ -28,8 +29,8 @@ $(document).ready(function(){
 		$("#ruby").attr("value", ruby)
 		$("#cobalt").attr("value", cobalt)
 		$("#emerald").attr("value", emerald)
-		$("#number").html("Match Me: " +numberRand)
-		$("#sum").html(" ");
+		$("#number").html("Match Me: " + numberRand)
+		$("#sum").html("Your Total: " + total);
 		$("#scoreW").html("Wins : " + wins)
 		$("#scoreL").html("Losses : " + losses)
 		}
@@ -40,6 +41,7 @@ $(document).ready(function(){
 			cobalt = randomizer(1, 12);
 			emerald = randomizer(1, 12);
 			sum = [];
+			total = 0;
 
 		console.log(numberRand, diamond, ruby, cobalt, emerald);
 		
@@ -48,7 +50,7 @@ $(document).ready(function(){
 		$("#cobalt").attr("value", cobalt)
 		$("#emerald").attr("value", emerald)
 		$("#number").html("Match Me: " +numberRand)
-		$("#sum").html(" ");
+		$("#sum").html("Your Total: " + total);
 		$("#scoreW").html("Wins : " + wins)
 		$("#scoreL").html("Losses : " + losses)
 		}
@@ -67,14 +69,14 @@ $(document).ready(function(){
 			setTimeout(function(){
 				alert("You Win!");
 				Reset();
-				}, 1000);
+				}, 200);
 			wins++;
 			$("#scoreW").html("Wins : " + wins)
 			}else if (total > numberRand){
 			setTimeout(function(){
 				alert("You Lose!");
 				Reset();
-			}, 500);
+			}, 200);
 			losses++;
 			$("#scoreL").html("Losses : " + losses)
 			}
