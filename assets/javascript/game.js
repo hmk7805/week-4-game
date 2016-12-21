acb69144ec57bf40b3a05e328834c76bbf993bd0
Hannah King <hmking.nc@gmail.com>
2016-12-20 21:35:11 -0500

    Logic and functionalit in place

---
+++ assets/javascript/game.js
@@ -0,0 +1,91 @@
+//random number generated
+//4 crystal buttons
+//when the game starts a random number is generated for each crystal
+//when a crystal is clicked it prints out the value to another area 
+//for all of the crystals clicked it updates that number to give the sum
+//logic, if the number sum 
+$(document).ready(function(){
+	fullReset()
+	var diamond;
+	var ruby;
+	var cobalt;
+	var emerald;
+	var sum = [];
+	var total = 0;
+	var numberRand;
+	var wins = 0;
+	var losses = 0;
+	
+	function randomizer(min, max) {
+	    	return Math.floor(Math.random() * (max - min + 1) + min);
+	  		}
+	function fullReset() {
+			numberRand = randomizer(19, 120);
+			diamond = randomizer(1, 12);
+			ruby = randomizer(1, 12);
+			cobalt = randomizer(1, 12);
+			emerald = randomizer(1, 12);
+			wins = 0;
+			losses = 0;
+			
+		console.log(numberRand, diamond, ruby, cobalt, emerald);
+		
+		$("#diamond").attr("value", diamond)
+		$("#ruby").attr("value", ruby)
+		$("#cobalt").attr("value", cobalt)
+		$("#emerald").attr("value", emerald)
+		$("#number").html("Match Me: " +numberRand)
+		$("#sum").html(" ");
+		$("#scoreW").html("Wins : " + wins)
+		$("#scoreL").html("Losses : " + losses)
+		}
+		function Reset() {
+			numberRand = randomizer(19, 120);
+			diamond = randomizer(1, 12);
+			ruby = randomizer(1, 12);
+			cobalt = randomizer(1, 12);
+			emerald = randomizer(1, 12);
+			sum = [];
+
+		console.log(numberRand, diamond, ruby, cobalt, emerald);
+		
+		$("#diamond").attr("value", diamond)
+		$("#ruby").attr("value", ruby)
+		$("#cobalt").attr("value", cobalt)
+		$("#emerald").attr("value", emerald)
+		$("#number").html("Match Me: " +numberRand)
+		$("#sum").html(" ");
+		$("#scoreW").html("Wins : " + wins)
+		$("#scoreL").html("Losses : " + losses)
+		}
+
+		$('body').on('click', 'img', function(){
+			total = 0;
+			var crystalNumber = this.getAttribute('value')
+			sum.push(crystalNumber);
+			console.log(sum);
+			for (var i = 0; i < sum.length; i++) {
+				total += sum[i] << 0;
+				}
+			console.log("total " + total)
+			$("#sum").html("Your Total: " + total);
+			if (total === numberRand){
+			setTimeout(function(){
+				alert("You Win!");
+				Reset();
+				}, 1000);
+			wins++;
+			$("#scoreW").html("Wins : " + wins)
+			}else if (total > numberRand){
+			setTimeout(function(){
+				alert("You Lose!");
+				Reset();
+			}, 500);
+			losses++;
+			$("#scoreL").html("Losses : " + losses)
+			}
+		  });
+		});
+
+
+
